b1a3e8ff843c27e44deb89ccfcd998a9730199cd
EC2 Default User <ec2-user@ip-172-31-57-140.ec2.internal>
2021-07-21 02:42:42 +0000

    アカウント編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
   get 'users/index'
+  get 'users/account'
   resources :rooms
   resources :reservations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
